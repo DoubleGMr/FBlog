8439704ab9f97b6c3431e81376002d3f7ec62614
Jasonly <yao1508728331@gmail.com>
2018-04-21 15:58:11 +0800

    complete static-pages

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+	@base_title = "It's time for Reading."
+  end
+  
+  test "should get index" do
+    get root_url
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+
+  test "should get about" do
+    get about_url
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+
+end
